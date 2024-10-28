8f9e2a96b470286ecca2674ec7e48a6531e93a9e
groflaw <romanphoon@gmail.com>
2024-10-28 13:34:49 -0300

    update users model

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,7 +1,7 @@
 const User = require("../models/user"); // Example model
 
 exports.createUser = async (req, res) => {
-  const newUser = new User(req.body);
+  const newUser = new User(req.body.personInfo);
   try {
     await newUser.save();
     res.status(201).json(newUser);
--- api/models/user.js
+++ api/models/user.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String },
   email: { type: String, required: true },
+  birthDay: { type: Date, required: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
