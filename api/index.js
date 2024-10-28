237a5f9ccfb156a8c6f57abc7dc075c840f9e590
groflaw <romanphoon@gmail.com>
2024-10-28 11:41:34 -0300

    Add the Router on the server

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db"); // Import your db connection
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -15,6 +16,8 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/users", userRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
