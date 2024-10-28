21c8930c02c170d6c109951bf7ac1018aaf81487
groflaw <romanphoon@gmail.com>
2024-10-28 15:12:48 -0300

    fix the req

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,9 +1,9 @@
 const User = require("../models/user"); // Example model
 
 exports.createUser = async (req, res) => {
-  console.log(req.body.personInfo);
+  console.log(req.body);
 
-  const newUser = new User(req.body.personInfo);
+  const newUser = new User(req.body);
   try {
     await newUser.save();
     res.status(201).json(newUser);
