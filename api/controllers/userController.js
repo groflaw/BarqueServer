eccdfb29379366a296c638fdfc3d1323185e31f0
groflaw <romanphoon@gmail.com>
2024-10-29 02:48:06 -0300

    test the login

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -24,24 +24,24 @@ exports.createUser = async (req, res) => {
 
 exports.loginUser = async (req, res) => {
   const { email, password } = req.params; // Get email and password from URL parameters
-  console.log(email, password);
-  // try {
-  //   const existingUser = await User.findOne({ email });
 
-  //   if (!existingUser) {
-  //     return res.status(400).json({ error: "Invalid email or password" });
-  //   }
+  try {
+    const existingUser = await User.findOne({ email });
 
-  //   const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!existingUser) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
 
-  //   if (!isMatch) {
-  //     return res.status(400).json({ error: "Invalid email or password" });
-  //   }
+    const isMatch = await bcrypt.compare(password, existingUser.password);
 
-  //   res.status(200).json({
-  //     existingUser,
-  //   });
-  // } catch (error) {
-  //   res.status(500).json({ error: "Server error" });
-  // }
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    res.status(200).json({
+      existingUser,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
 };
