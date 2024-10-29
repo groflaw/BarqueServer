25e8ef4cf7be16518986bca3cba65e530c0647c2
groflaw <romanphoon@gmail.com>
2024-10-29 02:01:34 -0300

    add the encrypt the password

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,13 +1,25 @@
-const User = require("../models/user"); // Example model
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
-  console.log(req.body);
-
   const newUser = new User(req.body);
+
   try {
+    const existingUser = await User.findOne({ email: newUser.email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+
+    const saltRounds = 10;
+    newUser.password = await bcrypt.hash(newUser.password, saltRounds);
+
     await newUser.save();
+
     res.status(201).json(newUser);
   } catch (error) {
     res.status(400).json({ error: "Could not create user" });
   }
 };
+
+exports.loginUser = async (req, res) => {};
