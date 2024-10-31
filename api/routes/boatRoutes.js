f709f7a37810f1ca1af34ec22ce5b7f0833ce76b
groflaw <romanphoon@gmail.com>
2024-10-31 04:36:45 -0300

    fix the add boat url

--- api/routes/boatRoutes.js
+++ api/routes/boatRoutes.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 const boatController = require("../controllers/boatController");
 router.get("/getalltype", boatController.getallboattype);
+router.post("/addboattype", boatController.addboattype);
 module.exports = router;
