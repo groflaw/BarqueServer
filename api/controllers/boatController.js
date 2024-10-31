9c5ce602a37674bb82b982184ae5b2d8dc154df5
groflaw <romanphoon@gmail.com>
2024-10-31 05:00:40 -0300

    update get all boat type

--- api/controllers/boatController.js
+++ api/controllers/boatController.js
@@ -2,8 +2,11 @@ const BoatType = require("../models/boattype");
 
 exports.getallboattype = async (req, res) => {
   try {
-    // const allboattypes = BoatType.find({});
-    res.json({ flag: true });
+    const allboattypes = await BoatType.find({});
+    res.json({
+      flag: true,
+      data: allboattypes,
+    });
   } catch (error) {
     res.json({
       flag: false,
