c5452baea9bdb6c717877709f76d44d72e28395b
groflaw <romanphoon@gmail.com>
2024-10-31 04:33:40 -0300

    add the boat type

--- api/controllers/boatController.js
+++ api/controllers/boatController.js
@@ -12,3 +12,19 @@ exports.getallboattype = async (req, res) => {
     });
   }
 };
+
+exports.addboattype = async (req, res) => {
+  try {
+    const newtype = new BoatType(req.body);
+
+    await newtype.save();
+
+    res.json({ flag: true, newtype });
+  } catch (error) {
+    res.json({
+      flag: false,
+      sort: "general",
+      error: "Could add boat type",
+    });
+  }
+};
