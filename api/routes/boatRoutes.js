b1b9d20281a49e1b3390811044f49c9776586813
groflaw <romanphoon@gmail.com>
2024-11-01 01:44:26 -0300

    fix the basic boat setting

--- api/routes/boatRoutes.js
+++ api/routes/boatRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const boatController = require("../controllers/boatController");
-router.get("/getalltype", boatController.getallboattype);
+const boatController = require("../controllers/boatController"); // Ensure this path is correct
+
 router.post("/addboattype", boatController.addboattype);
-router.post("/addbrand", boatController.addbrand);
-router.get("/getallbrand", boatController.getallbrand);
+router.get("/getallboattype", boatController.getallboattype);
+router.post("/addboatbrand", boatController.addboatbrand);
+router.get("/getallboatbrand", boatController.getallboatbrand);
+router.post("/setEnginesCount", boatController.setEnginesCount);
+router.get("/getEnginesCount", boatController.getEnginesCount);
+
 module.exports = router;
