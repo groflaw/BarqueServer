64de66ca456ab706fb0302a1785923da0015f3cc
groflaw <romanphoon@gmail.com>
2024-11-08 17:11:43 -0300

    test file upload

--- api/controllers/boatController.js
+++ api/controllers/boatController.js
@@ -457,19 +457,19 @@ exports.addDocImage = async (req, res) => {
 
   try {
     const boat = await Boat.findOne({ _id: boatId });
-    for (const file of files) {
-      const params = {
-        Bucket: process.env.S3_BUCKET,
-        Key: `boats/${boatId}/${Date.now()}_${file.originalname}`,
-        Body: file.buffer,
-        ContentType: file.mimetype,
-      };
-      const uploadResult = await s3.upload(params).promise();
-      if (["navigation", "authorization"].includes(imagetype)) {
-        boat.docImage[imagetype] = uploadResult.Location;
-      }
-      await boat.save();
-    }
+    // for (const file of files) {
+    //   const params = {
+    //     Bucket: process.env.S3_BUCKET,
+    //     Key: `boats/${boatId}/${Date.now()}_${file.originalname}`,
+    //     Body: file.buffer,
+    //     ContentType: file.mimetype,
+    //   };
+    //   const uploadResult = await s3.upload(params).promise();
+    //   if (["navigation", "authorization"].includes(imagetype)) {
+    //     boat.docImage[imagetype] = uploadResult.Location;
+    //   }
+    //   await boat.save();
+    // }
     res.json({ flag: true, data: boat });
   } catch (error) {
     console.error(error);
