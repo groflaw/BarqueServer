21b1e170d1478910172badba291370de66bb5a8f
groflaw <romanphoon@gmail.com>
2024-11-13 08:43:08 -0300

    setAvatar

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,14 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const AWS = require("aws-sdk");
 
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
 exports.createUser = async (req, res) => {
   const newUser = new User(req.body);
 
