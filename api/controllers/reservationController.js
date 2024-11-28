891e11be90d5c26dd9129091682c0016740ea736
groflaw <romanphoon@gmail.com>
2024-11-28 09:30:45 +0300

    add get reservation function

--- api/controllers/reservationController.js
+++ api/controllers/reservationController.js
@@ -1,13 +1,31 @@
 const Reservation = require("../models/reservation");
 const Boat = require("../models/boat");
 
-exports.saveReservation = async (req,res) =>{
-    try{
-        const newreservation = new Reservation(req.body);
-        await newreservation.save();
-        res.json({flag : true, data : newreservation})
-    }catch(error){
-        res.json({ flag: false, sort: "general", error: "Could not create reservation" });
-    }
-}
+exports.saveReservation = async (req, res) => {
+  try {
+    const newreservation = new Reservation(req.body);
+    await newreservation.save();
+    res.json({ flag: true, data: newreservation });
+  } catch (error) {
+    res.json({
+      flag: false,
+      sort: "general",
+      error: "Could not create reservation",
+    });
+  }
+};
 
+exports.getReservations = async (req, res) => {
+  try {
+    const reservations = Reservation.find({ userId: req.params.userId })
+      .populate("userId", "_id firstName lastName avatar")
+      .populate("boatId", "_id model description");
+    res.json({ flag: true, data : reservations });
+  } catch (error) {
+    res.json({
+      flag: false,
+      sort: "general",
+      error: "Could not create reservation",
+    });
+  }
+};
--- api/routes/reservation.js
+++ api/routes/reservation.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const reservationController = require("../controllers/reservationController")
 
 router.post("/save", reservationController.saveReservation);
+router.get("/getReservations/:userId",reservationController.getReservations)
 
 module.exports = router;
