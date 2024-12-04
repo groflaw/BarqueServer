92c8fabcd697b8efb6f5b292f3a05ed31592b326
groflaw <romanphoon@gmail.com>
2024-12-04 03:45:35 +0300

    add some field to reservation model, and return cancellation too

--- api/models/reservation.js
+++ api/models/reservation.js
@@ -5,7 +5,8 @@ const reservationSchema = new mongoose.Schema({
   hostId: { type: mongoose.Schema.ObjectId, ref: "User" }, // host
   boatId: { type: mongoose.Schema.ObjectId, ref: "boat" }, // boat
   planId: { type: Number }, // plan
-  status : {type : Number, default : 0}, // 0 : TobeConfirmed, 1 : Cancelled, 2 : Due, 3 : Confirmed. 
+  status : {type : Number, default : 0}, // 0 : TobeConfirmed, 1 : Cancelled, 2 : Due, 3 : Confirmed.
+  paymethod : {type : Number, default : 0}, // 0: cash,1: card,2: Zelle,3 : binance,4 : paypal, 5 : bank 
   date: { type: Date, default: Date.now } 
 });
 
