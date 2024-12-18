f480b0371900cf126e1345350f668e8fdc2df46e
groflaw <romanphoon@gmail.com>
2024-12-19 00:08:21 +0300

    add database and install nodemon

--- api/models/reservation.js
+++ api/models/reservation.js
@@ -6,7 +6,8 @@ const reservationSchema = new mongoose.Schema({
   boatId: { type: mongoose.Schema.ObjectId, ref: "boat" }, // boat
   planId: { type: Number }, // plan
   status : {type : Number, default : 0}, // 0 : TobeConfirmed, 1 : Cancelled, 2 : Due, 3 : Confirmed.
-  paymethod : {type : Number, default : 0}, // 0: cash,1: card,2: Zelle,3 : binance,4 : paypal, 5 : bank 
+  paymethod : {type : Number, default : 0}, // 0: cash,1: card,2: Zelle,3 : binance,4 : paypal, 5 : bank
+  confrimID : {type: String,}, 
   date: { type: Date, default: Date.now } 
 });
 
