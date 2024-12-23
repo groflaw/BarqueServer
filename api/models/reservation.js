e406f48acbe9ad52014a1dc48d39a3eaea3ff14f
groflaw <romanphoon@gmail.com>
2024-12-23 09:18:06 +0300

    add some field to reservation model

--- api/models/reservation.js
+++ api/models/reservation.js
@@ -5,10 +5,12 @@ const reservationSchema = new mongoose.Schema({
   hostId: { type: mongoose.Schema.ObjectId, ref: "User" }, // host
   boatId: { type: mongoose.Schema.ObjectId, ref: "boat" }, // boat
   planId: { type: Number }, // plan
-  status : {type : Number, default : 0}, // 0 : TobeConfirmed, 1 : Cancelled, 2 : Due, 3 : Confirmed.
-  paymethod : {type : Number, default : 0}, // 0: cash,1: card,2: Zelle,3 : binance,4 : paypal, 5 : bank
-  confrimID : {type: String,}, 
-  date: { type: Date, default: Date.now } 
+  count : {type : Number},
+  status: { type: Number, default: 0 }, // 0 : TobeConfirmed, 1 : Cancelled, 2 : Due, 3 : Confirmed.
+  paid: { type: Number },
+  paymethod: { type: Number, default: 0 }, // 0: cash,1: card,2: Zelle,3 : binance,4 : paypal, 5 : bank
+  confrimID: { type: String },
+  date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("reservation", reservationSchema);
